aed1ffbf56ce5bf30821584b2ade7bba5c046c79
simsononroad <d-github@kopaszhegy.hu>
2024-11-08 21:50:54 +0100

    save

--- idojaras.py
+++ idojaras.py
@@ -54,8 +54,66 @@ try:
             print(f"Mentett idők: {time}")
         #print(times)
         cur.execute("SELECT * FROM weather;")
-        citys = cur.fetchone()
-        print(citys)
+        data = cur.fetchall()
+        save_time = data[0][0]
+        city = data[1]
+        temp = data[0][2]
+        feels_like = data[0][3]
+        sky_description = data[0][4]
+        wind_speed = data[0][5]
+        humidity = data[0][6]
+
+        history_window = customtkinter.CTk()
+        history_window.geometry("1000x650")
+        history_window.title("Időjárás")
+        history_window.resizable(0, 10)
+        """message = customtkinter.CTkLabel(history_window, text=f"Város: {city}", font=("Helvetica", 18))
+        void = tk.Label(window, text=f"")
+        save_times = customtkinter.CTkLabel(history_window, text=f"Időjárás lementve: {save_time}", font=("Helvetica", 18))
+        tempareture = customtkinter.CTkLabel(history_window, text=f"A jelenlegi hőmérséklet: {temp} °C", font=("Helvetica", 18))
+        tempareture_feels = customtkinter.CTkLabel(history_window, text=f"A jelenlegi hőérzet: {feels_like} °C", font=("Helvetica", 18))
+        if sky_description == "clear sky":
+            sky = customtkinter.CTkLabel(history_window, text=f"Az ég tiszta", font=("Helvetica", 18))
+        if sky_description == "few clouds":
+            sky = customtkinter.CTkLabel(history_window, text=f"Néhány felhő van az égen", font=("Helvetica", 18))
+        else:
+            sky = customtkinter.CTkLabel(history_window, text=f"Az ég: {sky_description}", font=("Helvetica",18))
+        
+        if wind_speed == 0:
+            wind = customtkinter.CTkLabel(history_window, text=f"Jelenleg nincs szél", font=("Helvetica", 18))
+        else:
+            wind = customtkinter.CTkLabel(history_window, text=f"A szél sebessége: {wind_speed} m/s", font=("Helvetica", 18))
+        humadity = customtkinter.CTkLabel(history_window, text=f"A jelenlegi páratartartalom: {humidity} %", font=("Helvetica", 18))
+        message.pack()
+        save_times.pack()
+        message.place(relx=0.1, rely=0.2)
+        save_times.place(relx=0.1, rely=0.3)
+        tempareture.pack()
+        tempareture.place(relx=0.1, rely=0.4)
+        tempareture_feels.pack()
+        tempareture_feels.place(relx=0.1, rely=0.5)
+        sky.pack()
+        sky.place(relx=0.1, rely=0.6)
+        wind.pack()
+        wind.place(relx=0.1, rely=0.7)
+        humadity.pack()
+        humadity.place(relx=0.1, rely=0.8)"""
+
+        def reszletes():
+            for datas in data:
+                print(datas)
+        dark = customtkinter.CTkButton(master=history_window,
+                                    width=120,
+                                    height=32,
+                                    border_width=0,
+                                    corner_radius=10,
+                                    text="Részletes jelentés konzolon",
+                                    hover_color="lightblue",
+                                    command=reszletes)
+
+        dark.place(relx=0, rely=0)
+
+        history_window.mainloop()
         
         print(f"\n{gtime}")
 
@@ -79,7 +137,7 @@ try:
                                     hover_color="lightblue",
                                     command=dark)
 
-    dark.place(relx=0.14, rely=0)
+    dark.place(relx=0.16, rely=0)
     history = customtkinter.CTkButton(master=window,
                                     width=120,
                                     height=32,
@@ -89,7 +147,7 @@ try:
                                     hover_color="lightblue",
                                     command=history)
 
-    history.place(relx=0.1, rely=0)
+    history.place(relx=0, rely=0)
     text_entry = customtkinter.CTkEntry(master=window,
                                     placeholder_text="Település / Ország",
                                     width=200,
@@ -161,7 +219,7 @@ try:
                                     text="Adatok mentése!",
                                     hover_color="lightblue",
                                     command=save_data)
-        save.place(relx=0.14, rely=0.1)
+        save.place(relx=0.14, rely=0.3)
         
         
         def remover():
@@ -231,7 +289,6 @@ try:
                                     hover_color="lightblue",
                                     command=mapopen)
     mapbtn.place(relx=0.7, rely=0.2)
-
 
     window.mainloop()
 except KeyboardInterrupt:
